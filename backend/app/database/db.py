41c569d87903cb2cb1b740b5f97ca1c51fe2b2a4
Arnav Gulve <168359684+DemonKing023@users.noreply.github.com>
2025-03-09 05:08:40 +0530

    Add files via upload

---
+++ backend/app/database/db.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# Load datasets
+students_df = pd.read_csv("data/large_student_dataset.csv")
+mentors_df = pd.read_csv("data/large_mentor_dataset.csv")
+courses_df = pd.read_csv("data/udemy_courses_no_title.csv")
